ebcaae1a8f52d75dc5b25b3dffa1f76f12ad1ac3
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-10-03 16:00:25 +0300

    Starting DP

---
+++ OOPAllInOne/Design Patterns/Creational/Singleton/Logger.cs
@@ -0,0 +1,26 @@
+﻿namespace Singleton
+{
+    public class Logger
+    {
+        public int LogLevel { get; set; }
+        public void LogInfo() => Console.WriteLine($"{LogLevel} seviyesinde loga yazılıdı");
+
+
+        private static Logger instance;
+        public static Logger CreateLogger()
+        {
+            if (instance == null)
+            {
+                instance = new Logger();
+            }
+
+            return instance;
+
+        }
+        private Logger()
+        {
+
+        }
+
+    }
+}
---
+++ OOPAllInOne/Design Patterns/Creational/Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Singleton;
+
+Console.WriteLine("Hello, World!");
+/*
+ * PROBLEM:
+ * 
+ *    Bir nesnenin uygulama boyunca sadece ve sadece bir adet olmasını istiyorsunuz. Bunu nasıl sağlarsınız?
+ *    
+ *    
+ *    
+ */
+Logger logger = Logger.CreateLogger();
+logger.LogLevel = 5;
+logger.LogInfo();
+
+Logger logger1 = Logger.CreateLogger();
+Console.WriteLine(logger1.LogLevel);
+logger1.LogInfo();
